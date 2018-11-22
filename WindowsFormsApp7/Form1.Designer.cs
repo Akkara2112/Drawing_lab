b24ecf6e41c701f26982c2c3abc1d9029a384d71
Akkara <akkara2112@outlook.com>
2018-11-22 21:03:12 +0300

    add

---
+++ WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.графикаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкиКонтураToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.цветКонтураToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.цветЗаливкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.цветФонаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.печатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem,
+            this.графикаToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(517, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.печатьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // графикаToolStripMenuItem
+            // 
+            this.графикаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.настройкиКонтураToolStripMenuItem,
+            this.цветКонтураToolStripMenuItem,
+            this.цветЗаливкиToolStripMenuItem,
+            this.цветФонаToolStripMenuItem});
+            this.графикаToolStripMenuItem.Name = "графикаToolStripMenuItem";
+            this.графикаToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.графикаToolStripMenuItem.Text = "Графика";
+            // 
+            // настройкиКонтураToolStripMenuItem
+            // 
+            this.настройкиКонтураToolStripMenuItem.Name = "настройкиКонтураToolStripMenuItem";
+            this.настройкиКонтураToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.настройкиКонтураToolStripMenuItem.Text = "Настройки контура";
+            this.настройкиКонтураToolStripMenuItem.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // цветКонтураToolStripMenuItem
+            // 
+            this.цветКонтураToolStripMenuItem.Name = "цветКонтураToolStripMenuItem";
+            this.цветКонтураToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.цветКонтураToolStripMenuItem.Text = "Цвет контура";
+            this.цветКонтураToolStripMenuItem.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // цветЗаливкиToolStripMenuItem
+            // 
+            this.цветЗаливкиToolStripMenuItem.Name = "цветЗаливкиToolStripMenuItem";
+            this.цветЗаливкиToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.цветЗаливкиToolStripMenuItem.Text = "Цвет заливки";
+            this.цветЗаливкиToolStripMenuItem.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // цветФонаToolStripMenuItem
+            // 
+            this.цветФонаToolStripMenuItem.Name = "цветФонаToolStripMenuItem";
+            this.цветФонаToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.цветФонаToolStripMenuItem.Text = "Цвет фона";
+            this.цветФонаToolStripMenuItem.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(100, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе..";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(417, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Настройки контура";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(417, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 58);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Цвет контура фигуры";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(417, 150);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 58);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Цвет заливки фигуры";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(417, 214);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(79, 31);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Цвет фона";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(430, 323);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Сохранить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(398, 290);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 328);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 8;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(418, 262);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(77, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Анимация";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(349, 324);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Загрузить";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // печатьToolStripMenuItem
+            // 
+            this.печатьToolStripMenuItem.Name = "печатьToolStripMenuItem";
+            this.печатьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.печатьToolStripMenuItem.Text = "Печать";
+            this.печатьToolStripMenuItem.Click += new System.EventHandler(this.печатьToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 350);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem графикаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem настройкиКонтураToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem цветКонтураToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem цветЗаливкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem цветФонаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ToolStripMenuItem печатьToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing.Printing;
+
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        Bitmap b;
+        Graphics g;
+        public Color circuitColor;
+        public Color fillcolor;
+        public Color fillback;
+        public SolidBrush fill;
+        public int sizeofcir;
+        public Form1()
+        {
+            InitializeComponent();
+
+            SizeOfCircuit f1 = new SizeOfCircuit();                  
+            b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = b;
+            g = Graphics.FromImage(b);
+
+            circuitColor = new Color();
+            circuitColor = Color.Black;
+            fillcolor = new Color();
+            fillcolor = Color.White;
+            fillback = new Color();
+            fillback = Color.White;
+            pictureBox1.BackColor = fillback;
+         }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (checkBox1.Checked == false)
+            {  Point[] mypoint =
+                {
+                    new Point (e.X - 30, e.Y),
+                    new Point (e.X - 20, e.Y +20),
+                    new Point (e.X + 20 , e.Y+20),
+                    new Point (e.X + 30, e.Y),
+                    new Point (e.X +20, e.Y- 20 ),
+                    new Point (e.X -20, e.Y-20),
+                    new Point (e.X - 30, e.Y),
+                };
+            drawcont(mypoint);
+            }
+            if (checkBox1.Checked == true)
+            {
+                Point[] mypoint =
+                {
+                    new Point (e.X - 30, e.Y),
+                    new Point (e.X - 20, e.Y +20),
+                    new Point (e.X + 20 , e.Y+20),
+                    new Point (e.X + 30, e.Y),
+                    new Point (e.X +20, e.Y- 20 ),
+                    new Point (e.X -20, e.Y-20),
+                    new Point (e.X - 30, e.Y),
+                };
+                drawcont(mypoint);
+                pictureBox1.Image = null;
+            }
+        }
+
+        public void drawcont(Point[] p)
+        {           
+            g = Graphics.FromImage(b);
+            if (checkBox1.Checked)
+            { Thread.Sleep(5); g.Clear(fillback); }
+            g.DrawPolygon(new Pen(circuitColor, sizeofcir), p);
+            fill = new SolidBrush(fillcolor);
+            g.FillPolygon(fill, p);
+            pictureBox1.Image = b;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SizeOfCircuit f = new SizeOfCircuit();
+            f.ShowDialog();
+            sizeofcir = f.Size;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+                circuitColor = MyDialog.Color;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+                fillcolor = MyDialog.Color;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+                fillback = MyDialog.Color;
+            pictureBox1.BackColor = fillback;
+            pictureBox1.Image = b;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog st = new SaveFileDialog()
+            {
+                Filter = "Image files(*.bmp)|*.bmp"
+            };
+            Bitmap b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.DrawToBitmap(b, pictureBox1.ClientRectangle);
+
+            if (st.ShowDialog() == DialogResult.OK)
+            {
+                b.Save(st.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 box = new AboutBox1();
+            box.ShowDialog();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = "X:" + e.X + "\t" + "Y:" + e.Y;
+
+            if (checkBox1.Checked == true)
+            {
+                Point[] mypoint =
+                {
+                    new Point (e.X - 30, e.Y),
+                    new Point (e.X - 20, e.Y +20),
+                    new Point (e.X + 20 , e.Y+20),
+                    new Point (e.X + 30, e.Y),
+                    new Point (e.X +20, e.Y- 20 ),
+                    new Point (e.X -20, e.Y-20),
+                    new Point (e.X - 30, e.Y),
+                };
+                drawcont(mypoint);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Bitmap image; //Bitmap для открываемого изображения
+
+            OpenFileDialog open_dialog = new OpenFileDialog(); //создание диалогового окна для выбора файла
+            open_dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*"; //формат загружаемого файла
+            if (open_dialog.ShowDialog() == DialogResult.OK) //если в окне была нажата кнопка "ОК"
+            {
+                try
+                {
+                    image = new Bitmap(open_dialog.FileName);
+                    this.pictureBox1.Size = image.Size;
+                    pictureBox1.Image = image;
+                    pictureBox1.Invalidate();
+                }
+                catch
+                {
+                    DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void печатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Printing.PrintDocument myPrintDocument1 = new System.Drawing.Printing.PrintDocument();
+            PrintDialog myPrinDialog1 = new PrintDialog();
+            myPrintDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(myPrintDocument2_PrintPage);
+            myPrinDialog1.Document = myPrintDocument1;
+            if (myPrinDialog1.ShowDialog() == DialogResult.OK)
+            {
+                myPrintDocument1.Print();
+            }
+        }
+        private void myPrintDocument2_PrintPage(System.Object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Bitmap myBitmap1 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.DrawToBitmap(myBitmap1, new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
+            e.Graphics.DrawImage(myBitmap1, 0, 0);
+            myBitmap1.Dispose();
+        }
+    }
+}
